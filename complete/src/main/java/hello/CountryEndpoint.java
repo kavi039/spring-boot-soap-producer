749252bc3b908e2d30366f68aac2ad7b3266b31b
kavita.bora <kavita.bora@paytm.com>
2019-09-09 14:05:26 +0530

    countryenpoint added response data

--- complete/src/main/java/hello/CountryEndpoint.java
+++ complete/src/main/java/hello/CountryEndpoint.java
@@ -7,6 +7,7 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import io.spring.guides.gs_producing_web_service.Param0;
+import io.spring.guides.gs_producing_web_service.Param0.Reply;
 
 @Endpoint
 public class CountryEndpoint {
@@ -28,7 +29,27 @@ public class CountryEndpoint {
 
 		// response.getReply().getDataBlock().getMOBENQ().getData().setServiceProvider("adgjgef");
 		// response.setReply(countryRepository.findCountry("Spain"));
-
-		return request;
+		System.out.println(".................................. 1");
+		Param0 p = new Param0();
+		Reply.Header header = new Reply.Header();
+		header.setBatchId(request.getRequest().getHeader().getBatchId());
+		Reply.DataBlock dataBlock = new Reply.DataBlock();
+		Reply.DataBlock.MOBENQ mobenq = new Reply.DataBlock.MOBENQ();
+		Reply.DataBlock.MOBENQ.Data data = new Reply.DataBlock.MOBENQ.Data();
+		data.setActionId("ACtion1234");
+		data.setServiceProvider("DLH");
+		data.setContractType("FULL_TIME");
+		data.setRespCode("200");
+		data.setStatus("SUCCESS");
+		mobenq.setData(data);
+		dataBlock.setMOBENQ(mobenq);
+		Param0.Reply reply = new Param0.Reply();
+		reply.setHeader(header);
+		reply.setDataBlock(dataBlock);
+		p.setReply(reply);
+//		request.getReply().getDataBlock().getMOBENQ().getData()
+//				.setActionId(request.getRequest().getDataBlock().getMOBENQ().getData().getActionId());
+		System.out.println(".................................. 2");
+		return p;
 	}
 }
